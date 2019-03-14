fe5f3d30ae41917f4c2fc203405419ab1f942454
Nivedita <Nivedita@Nivedita>
2019-03-14 15:15:04 +0530

    Calculator program using switch case

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -5,14 +5,14 @@ public class Calculator {
     public static void main(String[] args)
     {
         System.out.println("Enter first and second number:");
-        Scanner inp= new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
         int num1,num2;
-        num1 = inp.nextInt();
-        num2 = inp.nextInt();
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
         int ans;
         System.out.println("Enter your selection: 1 for Addition, 2 for substraction 3 for Multiplication and 4 for division:");
         int choose;
-        choose = inp.nextInt();
+        choose = sc.nextInt();
         switch (choose){
         case 1:
             System.out.println(add( num1,num2));
@@ -39,6 +39,7 @@ public class Calculator {
     {
     	int result=0;
 //        write your code here 
+    	result=x+y;
     	
         return result;
     }
@@ -46,18 +47,21 @@ public class Calculator {
     {
     	int result=0;
 //        write your code here
+    	result=x-y;
         return result;
     }
     public static int mult(int x, int y)
     {
     	int result=0;
-//      write your code here        
+//      write your code here
+    	result=x*y;
     	return result;
     }
     public static int div(int x, int y)
     {
     	int result=0;
 //      write your code here
+    	result=x/y;
     	return result;
     }
 
